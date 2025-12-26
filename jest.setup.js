af2e6e45c1847d56ea48e5e32b4d346594b29cbd
pon-gitweb <thatguyponi@gmail.com>
2025-12-27 09:55:25 +1300

    chore: ignore backups in jest and mock AsyncStorage

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,6 @@
 // Keep setup lean; add custom matchers/mocks here when needed.
+
+jest.mock(
+  '@react-native-async-storage/async-storage',
+  () => require('@react-native-async-storage/async-storage/jest/async-storage-mock')
+);
