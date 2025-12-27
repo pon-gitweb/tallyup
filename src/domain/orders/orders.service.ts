ac0d45aae30f46dfe8e7b5c30d1ef09046326ac0
pon-gitweb <thatguyponi@gmail.com>
2025-12-28 12:12:24 +1300

    refactor(orders): route repo-owned methods through OrdersRepo in OrdersService

--- src/domain/orders/orders.service.ts
+++ src/domain/orders/orders.service.ts
@@ -4,12 +4,12 @@
  * Screens should import from `src/domain/orders` eventually.
  */
 import { runAISuggest as runAISuggestLegacy } from '../../services/orders/suggestAI';
+import { OrdersRepo } from './orders.repo';
 import { buildSuggestedOrdersInMemory as buildSuggestedOrdersInMemoryLegacy } from '../../services/orders/suggest';
 import {
   createDraftsFromSuggestions as createDraftsFromSuggestionsLegacy,
   computeSuggestionKey as computeSuggestionKeyLegacy,
 } from '../../services/orders/createFromSuggestions';
-import { listSubmittedOrders as listSubmittedOrdersLegacy } from '../../services/orders/listSubmittedOrders';
 
 export const OrdersService = {
   runAISuggest: runAISuggestLegacy,
@@ -18,5 +18,5 @@ export const OrdersService = {
   createDraftsFromSuggestions: createDraftsFromSuggestionsLegacy,
   computeSuggestionKey: computeSuggestionKeyLegacy,
 
-  listSubmittedOrders: listSubmittedOrdersLegacy,
+  listSubmittedOrders: OrdersRepo.listSubmittedOrders,
 };
