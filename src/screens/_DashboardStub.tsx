89192f5bdf83ba9dac2b829f8097de585086f143
pon-gitweb <thatguyponi@gmail.com>
2026-04-05 13:20:59 +1200

    chore: remove old anonymous package files and archived duplicate screens from git tracking

--- src/screens/_DashboardStub.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-export default function DashboardStub() {
-  return (
-    <View style={{ flex:1, alignItems:'center', justifyContent:'center' }}>
-      <Text>Dashboard (stub)</Text>
-    </View>
-  );
-}
